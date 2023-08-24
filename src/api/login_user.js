ca25d40fe58db9c8dc660d1bf6c15ba5d5a402a0
Diego Carrillo <61395884+DiegoCarrillo32@users.noreply.github.com>
2023-08-24 10:27:36 -0600

    first interaction with svelte
    testing components, async await, styles.

---
+++ src/api/login_user.js
@@ -0,0 +1,16 @@
+import { API_URL } from "../helpers/constants";
+
+export async function login (username = "", password = "") {
+    const result = await fetch(API_URL+"user/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Allow-Cross-Origin-Origin": "*"
+        },
+        body: JSON.stringify({
+            username,
+            password
+        })
+    })
+    return await result.json()
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Form from "../components/Form.svelte";
+    let  test = "Perritos"
+</script>
+<style>
+    .container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100vw;
+        height: 100vh;
+    }
+</style>
+<div class="container" >
+    <Form />
+</div>
